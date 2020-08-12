a1f7a454f7e4845256c1352101785b4aee93a357
davidshtian <david881223@outlook.com>
2020-08-12 12:17:12 +0800

    Update rename_table.py

--- rename_table.py
+++ rename_table.py
@@ -63,6 +63,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
+    # Parge args
     parser.add_argument('--database-name', type=str, default='default')
     parser.add_argument('--old-table-name', type=str, default='old_table')
     parser.add_argument('--new-table-name', type=str, default='new_table')
@@ -70,18 +71,23 @@ if __name__ == '__main__':
 
     args, _ = parser.parse_known_args()
 
+    # Get database, table and region info
     database_name = args.database_name
     old_table_name = args.old_table_name
     new_table_name = args.new_table_name
     region_name = args.region_name
 
+    # Print database, table and region info
     print(f'# Database: {database_name}')
     print(f'# Old Table: {old_table_name}')
     print(f'# New Table: {new_table_name}')
     print(f'# Region: {region_name}')
 
+    # Init boto3 client
     client = boto3.client("glue", region_name=args.region_name)
 
+    # Call the main function
     rename_glue_table(client, database_name, old_table_name, new_table_name)
 
+    # Completed. Yeah~
     print('# Completed.')
